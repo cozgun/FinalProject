2c28aad6533a60a806d45496205439fb5d5b8cd8
cozgun <59412630+cozgun@users.noreply.github.com>
2021-02-06 17:19:44 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        public List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product{ ProductID=1, CategoryID=1, ProductName="Bardak", UnitPrice=15, UnitsInStock=15},
+                new Product{ ProductID=2, CategoryID=1, ProductName="Kamera", UnitPrice=500, UnitsInStock=3},
+                new Product{ ProductID=3, CategoryID=2, ProductName="Telefon", UnitPrice=1500, UnitsInStock=2},
+                new Product{ ProductID=4, CategoryID=2, ProductName="Klavye", UnitPrice=150, UnitsInStock=65},
+                new Product{ ProductID=5, CategoryID=2, ProductName="Fare", UnitPrice=85, UnitsInStock=1}
+            };
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //LINQ - language integrated query 
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductID==product.ProductID);
+            _products.Remove(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public void Update(Product product)
+        {
+            //Gönderdiğim ürünID sine sahip olan listedeki ürünü (referansı aslında) bul.
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductID == product.ProductID);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryID = product.CategoryID;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock= product.UnitsInStock;
+        }
+
+        public List<Product> GetAllbyCategory(int categoryID)
+        {
+            return _products.Where(p => p.CategoryID == categoryID).ToList();
+
+        }
+    }
+}
