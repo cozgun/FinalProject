79b72d7ceb522bc465b26c429b1634b22757ad24
cozgun <59412630+cozgun@users.noreply.github.com>
2021-03-02 23:13:23 +0300

    12. gün yayın sonrası

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -18,15 +22,13 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
             //business codes
-            if (product.ProductName.Length < 2)
-            {
-                // magic strings
-                //return new ErrorResult("Ürün ismi minimum 2 karakter olmalıdır");
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            //validation codes  -  validation kodları ile business kodları birbirine karıştırmayalım.
+
+            ValidationTool.Validate(new ProductValidator(), product);
             
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
