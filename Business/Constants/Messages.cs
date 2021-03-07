1b3c2ae24920765395e1a3fe06e94a349c5525f0
cozgun <59412630+cozgun@users.noreply.github.com>
2021-03-07 21:35:27 +0300

    14. gün yayın sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -15,6 +17,13 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Kategoride 10'dan fazla ürün olamaz";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
         public static string CategoryLimitExceeded = "Bu isimde zaten başka bir ürün var";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserRegistered = "Kullanıcı kaydedildi";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError="Şifre hatalı";
+        public static string SuccessfulLogin = "Login başarılı";
+        public static string UserAlreadyExists = "Kullanıcı zaten mevcut";
+        public static string AccessTokenCreated = "Token oluşturuldu";
         //public static string ProductAdded = "Ürün eklendi";
         //public static string ProductAdded = "Ürün eklendi";
 
